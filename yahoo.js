7ad554c75fb65849f4877d0bf7fe5f66e14fc082
dCOMMICS <sanwurld@gmail.com>
2023-10-31 22:17:21 +0300

    💪🏿🚀

---
+++ yahoo.js
@@ -0,0 +1,15 @@
+var your_drink;
+
+var reverse = function(s){
+    return s.split(' ').reverse().join(' ');
+}
+
+var barista ={
+    str1: "ion",
+    str2: reverse ("rscne"),
+    str3: "ypt",
+    request: function(preferences) {
+        return preferences + "Secret word:" + this.str2 + this.str3 + this.str1;
+    }
+};
+barista.request (your_drink)

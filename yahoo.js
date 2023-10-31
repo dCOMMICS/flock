b338c5367ac51e682823c832f3d8273a5c2fb0f8
dCOMMICS <sanwurld@gmail.com>
2023-10-31 22:35:50 +0300

    💐🚀

--- yahoo.js
+++ yahoo.js
@@ -8,10 +8,10 @@ var barista ={
 
     str1: "ion",
     str2: reverse ("rscne"),
-    
+
     str3: "ypt",
     request: function(preferences) {
         return preferences + "Secret word:" + this.str2 + this.str3 + this.str1;
     }
 };
-barista.request (your_drink)
+barista.request (your_drink).replace
